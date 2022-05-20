39b7679884f1fe57c5ed3c45cab13f3a206a88d0
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-05-20 20:02:29 -0300

    feat: express-rescue adicionada as rotas

--- src/routes/loginRoutes/index.js
+++ src/routes/loginRoutes/index.js
@@ -1,7 +1,8 @@
 const route = require('express').Router();
+const rescue = require('express-rescue');
 const loginController = require('../../controllers/loginController');
 const validateLogin = require('../../middlewares/validateLogin');
 
-route.post('/', validateLogin, loginController.login);
+route.post('/', rescue(validateLogin), rescue(loginController.login));
 
 module.exports = route;
