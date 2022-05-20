95150f1dc48735861e5fecad6e736f98e6222818
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-05-20 20:18:16 -0300

    refact: model de User refatorada para não receber um timestamps

--- src/database/models/user.js
+++ src/database/models/user.js
@@ -1,10 +1,16 @@
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-    id: DataTypes.INTEGER,
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+    },
     displayName: DataTypes.STRING,
     email: DataTypes.STRING,
     password: DataTypes.STRING,
     image: DataTypes.STRING,
+  },
+  {
+    timestamps: false,
   });
 
   return User;
