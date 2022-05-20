d08eac150d8dc8e9afe3f36fdfa9707a139b6428
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-05-20 19:59:21 -0300

    feat: middleware de erro criado e adicionado a api

--- src/api.js
+++ src/api.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const routes = require('./routes');
+const errorHandler = require('./middlewares/errorHandler');
 
 const app = express();
 app.use(express.json());
 
 app.use(routes);
-
-// ...
+app.use(errorHandler);
 
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
