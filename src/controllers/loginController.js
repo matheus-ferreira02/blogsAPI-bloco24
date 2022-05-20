552d9cb180fdc8bcda215058412b8297d60e0d4d
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-05-20 20:17:26 -0300

    refact: loginController adaptada para receber um token

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,5 +1,10 @@
-const login = (req, res) => {
-  const token = 'ola';
+const loginServices = require('../services/loginService');
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  const token = await loginServices.login(email, password);
+  
   return res.status(200).json({ token });
 };
 
