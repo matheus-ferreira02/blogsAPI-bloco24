7b23f6acb7592538d225d72e9497bdf5d3a4d07c
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-05-20 20:28:10 -0300

    fix: função generateToken da utils

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 module.exports = (email) => {
   const jwtConfig = {
     expiresIn: '7d',
-    algotithm: 'HS256',
+    algorithm: 'HS256',
   };
 
   const token = jwt.sign({ data: email }, process.env.JWT_SECRET, jwtConfig);
