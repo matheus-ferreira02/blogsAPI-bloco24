020c8164b3fbc5333e169d85ec1c633d82abbd04
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-05-20 23:53:05 -0300

    fix: erro consertado por nao passar a senha para as services

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,9 +1,9 @@
 const loginServices = require('../services/loginService');
 
 const login = async (req, res) => {
-  const { email } = req.body;
+  const { email, password } = req.body;
 
-  const token = await loginServices.login(email);
+  const token = await loginServices.login(email, password);
   
   return res.status(200).json({ token });
 };
