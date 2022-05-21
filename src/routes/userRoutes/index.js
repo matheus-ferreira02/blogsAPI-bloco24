2d55c8f6459cbcea857095bc84810f29ccdd186c
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-05-20 21:40:37 -0300

    feat: middleware validateCreateUser adicinado a rota POST /user

--- src/routes/userRoutes/index.js
+++ src/routes/userRoutes/index.js
@@ -1,7 +1,8 @@
 const routes = require('express').Router();
 const rescue = require('express-rescue');
 const UserController = require('../../controllers/userController');
+const validateCreateUser = require('../../middlewares/validateCreateUser');
 
-routes.post('/', rescue(UserController.createUser));
+routes.post('/', rescue(validateCreateUser), rescue(UserController.createUser));
 
 module.exports = routes;
