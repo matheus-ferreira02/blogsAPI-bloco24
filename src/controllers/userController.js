2e6820c22a7eec654b1fb7a30cb269a0ce1c4916
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-05-20 23:17:12 -0300

    feat: controller getUsers criada

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,13 @@
 const userService = require('../services/userService');
 
+const getUsers = async (req, res) => {
+  const { authorization } = req.headers;
+
+  const users = userService.getUsers(authorization);
+
+  return res.status(200).json(users);
+};
+
 const createUser = async (req, res) => {
   const { displayName, email, password, image } = req.body;
 
@@ -10,4 +18,5 @@ const createUser = async (req, res) => {
 
 module.exports = {
   createUser,
+  getUsers,
 };
