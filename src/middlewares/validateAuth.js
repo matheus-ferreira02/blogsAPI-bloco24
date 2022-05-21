0d534ad715a080b89267b4e6910beaafb99d3a15
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-05-20 23:55:25 -0300

    refact: middleware de validação de token refatorada para um código mais limpo

--- src/middlewares/validateAuth.js
+++ src/middlewares/validateAuth.js
@@ -1,9 +1,17 @@
 const createObjError = require('../utils/createObjError');
+const decodeToken = require('../utils/decodeToken');
 
 module.exports = (req, _res, next) => {
   const { authorization } = req.headers;
+  console.log(authorization);
 
-  if (!authorization) throw createObjError(401, 'Token is required');
+  if (!authorization) throw createObjError(401, 'Token not found');
+
+  const decoded = decodeToken(authorization);
+
+  if (!decoded.data) throw createObjError(401, 'Expired or invalid token');
+
+  req.data = decoded.data;
 
   next();
 };
