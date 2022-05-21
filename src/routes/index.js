e73a068ce6391f55cdc1298556022bdcead97bdb
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-05-21 14:25:53 -0300

    feat: rota de categorias adicionada a api

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 const route = require('express').Router();
 const loginRoutes = require('./loginRoutes');
 const userRoutes = require('./userRoutes');
+const categoriesRoutes = require('./categoriesRoutes');
 
 route.use('/login', loginRoutes);
 route.use('/user', userRoutes);
+route.use('/categories', categoriesRoutes);
 
 module.exports = route;
