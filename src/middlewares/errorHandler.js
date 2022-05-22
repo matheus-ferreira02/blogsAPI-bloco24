99fa09689c6df827ebca5fd4e56439d71720da02
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-05-22 02:02:27 -0300

    feat: rota GET /post/search adicionada a api

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,5 +1,4 @@
 module.exports = (err, _req, res, _next) => {
-  console.log(err);
   if (err.statusCode) return res.status(err.statusCode).json({ message: err.message });
 
   return res.status(500).json({ message: 'Internal Server Error' });
--- src/routes/blogPostRoutes/index.js
+++ src/routes/blogPostRoutes/index.js
@@ -8,6 +8,9 @@ const validatePostUpdate = require('../../middlewares/validatePostUpdate');
 routes.post('/', rescue(validateAuth), rescue(validatePost), rescue(blogPostController.createPost));
 
 routes.get('/', rescue(validateAuth), rescue(blogPostController.getPosts));
+
+routes.get('/search', rescue(validateAuth), rescue(blogPostController.searchPost));
+
 routes.get('/:id', rescue(validateAuth), rescue(blogPostController.getPostById));
 
 routes.put('/:id',
