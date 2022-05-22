899801c1233012222ca7bddc7fdab396d12517a8
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-05-21 22:03:49 -0300

    feat: middleware de validação do post editado

--- src/middlewares/validatePostUpdate.js
+++ src/middlewares/validatePostUpdate.js
@@ -2,9 +2,9 @@ const schemaPost = require('../joi/schemaPost');
 const createObjError = require('../utils/createObjError');
 
 module.exports = (req, _res, next) => {
-  const { title, content, categoryIds } = req.body;
+  const { title, content } = req.body;
 
-  const { error } = schemaPost.validate({ title, content, categoryIds });
+  const { error } = schemaPost.validate({ title, content });
 
   if (error) throw createObjError(400, 'Some required fields are missing');
 
