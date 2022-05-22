3729871a9a4e937a88fadd4d7408920ba74d5e76
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-05-21 22:09:08 -0300

    feat: validador do corpo do posta a editar adicionado a rota

--- src/middlewares/validatePostUpdate.js
+++ src/middlewares/validatePostUpdate.js
@@ -1,10 +1,10 @@
-const schemaPost = require('../joi/schemaPost');
+const schemaPostUpdate = require('../joi/schemaPostUpdate');
 const createObjError = require('../utils/createObjError');
 
 module.exports = (req, _res, next) => {
   const { title, content } = req.body;
 
-  const { error } = schemaPost.validate({ title, content });
+  const { error } = schemaPostUpdate.validate({ title, content });
 
   if (error) throw createObjError(400, 'Some required fields are missing');
 
--- src/routes/blogPostRoutes/index.js
+++ src/routes/blogPostRoutes/index.js
@@ -3,12 +3,14 @@ const rescue = require('express-rescue');
 const validateAuth = require('../../middlewares/validateAuth');
 const blogPostController = require('../../controllers/blogPostController');
 const validatePost = require('../../middlewares/validatePost');
+const validatePostUpdate = require('../../middlewares/validatePostUpdate');
 
 routes.post('/', rescue(validateAuth), rescue(validatePost), rescue(blogPostController.createPost));
 
 routes.get('/', rescue(validateAuth), rescue(blogPostController.getPosts));
 routes.get('/:id', rescue(validateAuth), rescue(blogPostController.getPostById));
 
-routes.put('/:id', rescue(validateAuth), rescue(blogPostController.updatePost));
+routes.put('/:id',
+  rescue(validateAuth), rescue(validatePostUpdate), rescue(blogPostController.updatePost));
 
 module.exports = routes;
