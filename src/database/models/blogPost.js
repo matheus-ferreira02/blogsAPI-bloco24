13765517a08b29cd8be59af5924a1c88d739db83
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-05-24 13:48:53 -0300

    refact: model de BlogPost alterada para o nome correto

--- src/database/models/blogPost.js
+++ src/database/models/blogPost.js
@@ -1,5 +1,5 @@
 module.exports = (sequelize, DataTypes) => {
-  const Category = sequelize.define('BlogPost', {
+  const BlogPost = sequelize.define('BlogPost', {
     id: {
       autoIncrement: true,
       primaryKey: true,
@@ -16,13 +16,13 @@ module.exports = (sequelize, DataTypes) => {
     timestamps: false,
   });
 
-  Category.associate = ({ User }) => {
-    Category.belongsTo(User, {
+  BlogPost.associate = ({ User }) => {
+    BlogPost.belongsTo(User, {
       foreignKey: 'userId',
       as: 'user',
       onDelete: 'CASCADE'
     });
   }
 
-  return Category;
+  return BlogPost;
 }
